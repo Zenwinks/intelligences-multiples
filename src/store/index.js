39680877ce6b6b8fd112f6684eadc27495fe731d
Zenwinks <Lulu180299>
2021-06-16 10:59:17 +0200

    modification des questions

--- src/store/index.js
+++ src/store/index.js
@@ -6,84 +6,100 @@ Vue.use(Vuex)
 export default new Vuex.Store({
   state: {
     verboLing: [
-      "J'aime les jeux de mots et autres blagues du genre",
-      "Je me sens à l'aise dans le monde de la langue et des mots et j'en tire un renforcement positif",
-      "J'aime faire des mots croisés et jouer à des jeux",
-      "Je me rappelle mot pour mot ce que les gens me disent",
-      "J’aime participer à des débats ou à des discussions",
-      "Je préfère les questions à développer que les questions à choix multiples",
-      "J’aime tenir un journal ou écrire des histoires ou des articles",
-      "J’aime beaucoup lire"
+      "J’apprécie les jeux de mots.",
+      "J’ai une bonne mémoire pour les noms de personnes, de lieux, les dates ou les détails.",
+      "J’aime résoudre des mots croisés ou jouer au Scrabble.",
+      "J’ai de la facilité à écrire.",
+      "Je m’exprime avec un vocabulaire riche.",
+      "J’ai une assez bonne mémoire par rapport à ce que je lis ou par rapport à ce que j’entends.",
+      "Je suis attentif (ve) lorsque j’écoute une conférence, un exposé.",
+      "Le fait de prendre des notes m’aide à me comprendre et à mémoriser.",
+      "J’aime lire pendant mes temps libres.",
+      "J’aime raconter des histoires et des farces."
     ],
     logiqueMath: [
-      "Je travaille mieux quand mon plan de travail est bien organisé",
-      "J’aime les mathématiques et les sciences",
-      "Je garde une liste des choses à faire",
-      "J’aime jouer à des jeux de réflexion ou qui font appel à la pensée logique comme Jeopardy! et Cluedo",
-      "J’aime savoir le pourquoi des choses et chercher des éclaircissements aux questions qui m’intéressent",
-      "Je travaille mieux avec un agenda ou un calendrier",
-      "Je saisis rapidement les relations de cause à effet",
-      "Mes estimations sont souvent bonnes"
+      "J’adore résoudre des énigmes ou faire des casse-tête qui demandent de la logique.",
+      "J’aime classifier et catégoriser.",
+      "Je peux passer des heures à tenter de résoudre des problèmes.",
+      "Avant de me décider à quelque chose, je pèse le pour et le contre.",
+      "Je suis habile aux jeux de stratégie et je gagne souvent.",
+      "Lorsque j’étais enfant ou adolescent(e), j’avais un kit de chimie ou un autre kit de science avec lesquel j’aimais faire des expériences.",
+      "Le fait que je sois structuré(e) contribue au succès de ce que j’entreprends.",
+      "Je compte rapidement dans ma tête.",
+      "Je trouve rapidement les failles dans le raisonnement des gens.",
+      "Je me pose beaucoup de questions sur le fonctionnement des objets."
     ],
     visioSpatiale: [
-      "Je comprends les combinaisons de couleurs et vois quelles couleurs vont bien ensemble",
-      "J’aime faire des puzzles, des labyrinthes ou des jeux de patience",
-      "Je n’ai pas de mal à lire les cartes",
-      "J’ai un bon sens de l’orientation",
-      "Dans les films, je m’intéresse particulièrement aux scènes et aux activités",
-      "Quand je dors, mes rêves me paraissent très réels",
-      "Je peux prévoir les mouvements et les conséquences dans un plan de jeu (par exemple au hockey, aux échecs)",
-      "J’ai une mémoire surtout visuelle"
+      "Je peux facilement reconnaître la rotation d’une figure géométrique dans l’espace.",
+      "J’aime les exercices de visualisation. Quand je songe à réaménager une pièce, je peux facilement la voir dans ma tête.",
+      "J’aime visionner des films, des diapositives et regarder des photographies.",
+      "Je m’oriente facilement dans une nouvelle ville.",
+      "Lorsqu’un livre est illustré, je m’intéresse d’abord et surtout aux illustrations.",
+      "Je dessine des objets et des personnes de façon précise.",
+      "Je lis des cartes, des tableaux et des diagrammes sans difficulté.",
+      "Je tiens ma maison ou mon bureau en ordre : une place pour chaque chose; chaque chose à sa place.",
+      "Je vois des images dans ma tête quand je pense à quelque chose.",
+      "Je passe beaucoup de temps libre à dessiner."
     ],
     interperso: [
-      "Je travaille mieux quand j’interagis avec les autres",
-      "Je préfère les sports d’équipe aux sports individuels",
-      "La présence des autres me donne de l’énergie",
-      "Je préfère faire des choses en groupe plutôt qu’être seul",
-      "J’aime apprendre à connaître les autres cultures",
-      "J’ai l’habitude de parler de mes problèmes personnels avec mes amis",
-      "J’aime partager mes idées et mes sentiments avec d’autres",
-      "Je travaille mieux au sein d’un groupe où je peux discuter avec les autres de diverses questions"
+      "Je participe à des clubs sociaux ou sportifs.",
+      "J’aime m’exprimer dans les discussions de famille.",
+      "J’organise souvent des activités dans mon entourage.",
+      "Je suis à l’écoute des sentiments des autres; j’en tiens compte.",
+      "J’aime sortir pour rencontrer des amis.",
+      "J’aime bavarder sur tout et sur rien.",
+      "Je suis celui que l’on consulte lorsqu’il y a un conflit dans un groupe.",
+      "Je me plais à jouer aux cartes ou aux jeux de société.",
+      "Je semble être une personne populaire.",
+      "J’offre spontanément de l’aide à mes amis lorsqu’ils en ont besoin."
     ],
     intraperso: [
-      "J’aime ma solitude et avoir un monde à moi",
-      "J’ai quelques amis proches",
-      "J’ai des opinions bien arrêtées sur des questions controversées",
-      "Je travaille mieux quand je peux le faire à mon rythme",
-      "Je ne me laisse pas influencer facilement par les autres",
-      "Je comprends bien ce que je ressens et comment je réagis aux circonstances",
-      "Je soulève souvent des questions sur les valeurs et les croyances",
-      "Je me sais responsable de mon comportement"
+      "J’aime me retrouver seul(e) pour poursuivre mes intérêts personnels.",
+      "Je travaille bien seul(e).",
+      "Je me plais à penser à ma vie, à mes désirs et à mes croyances.",
+      "Je suis entreprenant(e).",
+      "J’ai confiance en moi.",
+      "Je réagis fortement aux opinions controversées.",
+      "Je décide par moi-même ce que je pense, ce que je choisis, ce que je fais.",
+      "J’ai besoin de savoir pourquoi je devrais faire quelque chose avant d’accepter de la faire.",
+      "Je suis motivé(e) à travailler seul dans certains projets.",
+      "Je suis indépendant(e) et je tiens à mes idées."
     ],
     corpoKine: [
-      "J’aime bouger, taper du pied ou me remuer quand je suis assis",
-      "J’aime les sports aux sensations extrêmes (comme le kayak de mer, la planche à neige, le vélo de montagne)",
-      "Je suis curieux sur le plan des sensations et j’aime toucher les objets pour en sentir la texture",
-      "J’ai une bonne coordination",
-      "J’aime le travail manuel",
-      "Je préfère participer physiquement plutôt que de rester assis et de regarder",
-      "Je comprends mieux les choses si je les fais (les touche, les bouge ou interagis avec elles)",
-      "J’aime créer, faire des choses avec mes mains"
+      "Il m’est difficile de rester assis trop longtemps; j’ai besoin de bouger.",
+      "Je peux mimer les gestes, les manières et les comportements d’autres personnes.",
+      "J’aime monter et démonter des objets.",
+      "J’aime les films qui procurent des sensations fortes !",
+      "Je touche les objets lorsque je me promène ou lorsque je me déplace dans la maison.",
+      "J’apprends en faisant.",
+      "Je suis habile dans la pratique de plusieurs sports.",
+      "Je suis habile de mes mains; j’aime travailler avec des outils ou des instruments.",
+      "J’ai besoin de toucher les gens lorsque je leur parle.",
+      "Je m’adonne à des activités physiques de façon régulière."
     ],
     musicale: [
-      "Je joue de la musique dans ma tête",
-      "Pour me rappeler des choses, je compose des comptines",
-      "Il m’est facile de suivre le rythme d’une musique",
-      "J’aime mettre une musique à des chansons ou des poèmes",
-      "Quand une musique joue, je bats la cadence",
-      "Je perçois les fausses notes",
-      "Je trouve facile de m’engager dans des activités musicales",
-      "Je suis fier de mes réalisations musicales"
+      "Je suis sensible à la musicalité des poèmes, des textes, de la parole.",
+      "Je chante juste ou je joue d’un instrument de musique.",
+      "J’aime les concerts, les récitals, les comédies musicales ou l’opéra.",
+      "J’ai de la difficulté à me concentrer sur un travail lorsque j’écoute la radio ou la télévision.",
+      "Je m’intéresse à toutes sortes de musiques; j’en écoute régulièrement, à la radio ou sur disque.",
+      "Je reconnais les fausses notes dans l’exécution d’une pièce musicale.",
+      "Je peux suivre la mesure dans une pièce musicale.",
+      "Il m’est facile de bouger ou de danser avec rythme.",
+      "Je me rappelle facilement les mélodies que j’entends.",
+      "Je suis sensible aux bruits et aux sons."
     ],
     naturaliste: [
-      "J’ai une collection de coquillages, de tasses, de pierres, de cartes de hockey, etc",
-      "Je remarque les similarités et les différences dans les arbres, les fleurs et autres objets de la nature",
-      "Je contribue activement à la protection de l’environnement",
-      "J’aime faire des fouilles et découvrir des objets et d’autres choses inhabituelles",
-      "Je préfère être à l’extérieur qu’à l’intérieur",
-      "J’aime faire des plantations et prendre soin d’un jardin",
-      "J’aime pêcher et suivre des animaux à la piste",
-      "La meilleure façon d’apprendre pour moi est d’aller en excursion, de voir des expositions sur la nature, etc"
+      "Je possède un télescope, des jumelles ou un microscope.",
+      "J’aime les randonnées de plein air, la chasse ou la pêche.",
+      "Je considère qu’il est important de préserver nos parcs nationaux. J’aime fréquenter les parcs Nature.",
+      "J’ai le « pouce vert ».",
+      "J’aime collectionner des objets puis les classer.",
+      "J’aime être en contact avec les animaux ou les observer dans leur environnement.",
+      "Je me préoccupe de l’environnement dans mes gestes quotidiens (recyclage, usage des ressources).",
+      "J’aime identifier les oiseaux, les plantes, les arbres.",
+      "Le contact avec la nature m’apaise, me calme.",
+      "Je m’intéresse à l’horticulture."
     ],
     results: [
       {label: 'Verbo-Linguistique', result: 0},
@@ -136,78 +152,78 @@ export default new Vuex.Store({
     getInfos: function (state) {
       return state.infos
     },
-    getSnackbar(state) {
+    getSnackbar (state) {
       return state.newSnack
     },
   },
   mutations: {
-    setVerboLing(state, value) {
+    setVerboLing (state, value) {
       state.verboLing = value
     },
-    setLogiqueMath(state, value) {
+    setLogiqueMath (state, value) {
       state.logiqueMath = value
     },
-    setVisioSpatiale(state, value) {
+    setVisioSpatiale (state, value) {
       state.visioSpatiale = value
     },
-    setInterperso(state, value) {
+    setInterperso (state, value) {
       state.interperso = value
     },
-    setIntraperso(state, value) {
+    setIntraperso (state, value) {
       state.intraperso = value
     },
-    setCorpoKine(state, value) {
+    setCorpoKine (state, value) {
       state.corpoKine = value
     },
-    setMusicale(state, value) {
+    setMusicale (state, value) {
       state.musicale = value
     },
-    setNaturaliste(state, value) {
+    setNaturaliste (state, value) {
       state.naturaliste = value
     },
-    setResults(state, value) {
+    setResults (state, value) {
       state.results = value
     },
-    setInfos(state, value) {
+    setInfos (state, value) {
       state.infos = value
     },
-    setSnackBar(state, newSnack) {
+    setSnackBar (state, newSnack) {
       state.newSnack.type = newSnack.type
       state.newSnack.text = newSnack.text
     },
   },
   actions: {
-    setVerboLing({commit}, content) {
+    setVerboLing ({commit}, content) {
       commit('setVerboLing', content)
     },
-    setLogiqueMath({commit}, content) {
+    setLogiqueMath ({commit}, content) {
       commit('setLogiqueMath', content)
     },
-    setVisioSpatiale({commit}, content) {
+    setVisioSpatiale ({commit}, content) {
       commit('setVisioSpatiale', content)
     },
-    setInterperso({commit}, content) {
+    setInterperso ({commit}, content) {
       commit('setInterperso', content)
     },
-    setIntraperso({commit}, content) {
+    setIntraperso ({commit}, content) {
       commit('setIntraperso', content)
     },
-    setCorpoKine({commit}, content) {
+    setCorpoKine ({commit}, content) {
       commit('setCorpoKine', content)
     },
-    setMusicale({commit}, content) {
+    setMusicale ({commit}, content) {
       commit('setMusicale', content)
     },
-    setNaturaliste({commit}, content) {
+    setNaturaliste ({commit}, content) {
       commit('setNaturaliste', content)
     },
-    setResults({commit}, content) {
+    setResults ({commit}, content) {
       commit('setResults', content)
     },
-    setInfos({commit}, content) {
+    setInfos ({commit}, content) {
       commit('setInfos', content)
     },
-    setSnackBar({commit}, newState) {
+    setSnackBar ({commit}, newState) {
       commit('setSnackBar', newState)
     },
   },
